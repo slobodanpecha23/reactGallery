ab47c838bf3b3235169bcffb67845f32c0ad027b
Slobodan <slobodan.mitrovic1988@gmail.com>
2020-12-20 22:46:58 +0100

    Added some improvement

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,10 +5,15 @@ export class SearchBar extends Component {
         term: ''
     }
 
+    onFormSubmit = (e) => {
+        e.preventDefault();
+        this.props.onSubmit(this.state.term)
+    }
+
     render() {
         return (
             <div className="ui segment">
-                <form className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label>Image search</label>
                         <input 
